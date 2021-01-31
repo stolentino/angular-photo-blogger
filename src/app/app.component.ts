a0b22de423e996ea273b361250e46fb8c88df12b
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-01-31 00:00:59 -0800

    add event to display random emoji

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     emoji = ['🎉', '😍', '😜', '👍'];
+    activeEmoji: string;
+    changeEmoji(){
+        this.activeEmoji = this.emoji[Math.floor(Math.random() * this.emoji.length)];
+    }
 
 }
