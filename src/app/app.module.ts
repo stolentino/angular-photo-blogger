acb02253affe9cad32111538d63032ab42384237
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-02-21 23:57:32 -0800

    create servive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { EntryListComponent, EntryComponent } from './entries';
+import { EntryListComponent, EntryComponent, EntryService } from './entries';
 
 
 @NgModule({
@@ -10,6 +10,7 @@ import { EntryListComponent, EntryComponent } from './entries';
         BrowserModule,
         HttpModule
     ],
+    providers: [ EntryService ],
     declarations: [
         AppComponent,
         EntryComponent,
