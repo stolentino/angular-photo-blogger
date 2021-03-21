12e89bdac9317b5ed7e20bd7380172f4d91ea99d
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-03-20 17:30:17 -0700

    post data using a service

--- src/app/entries/shared/entry.service.ts
+++ src/app/entries/shared/entry.service.ts
@@ -8,6 +8,11 @@ export class EntryService {
 
     }
 
+    addComment(entryId: number, comment: { name: string; comment: string; }){
+        return this.http.post(`/app/entries/${entryId}/comments`, comment)
+            .toPromise();
+    }
+
     getEntries(): Promise<Entry[]> {
         return this.http.get('/app/entries')
             .toPromise()
