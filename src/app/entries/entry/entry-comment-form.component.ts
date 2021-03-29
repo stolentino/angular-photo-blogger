b2c4e9f3f5ddd019fd61b407c3a6fd9bd852c9ff
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-03-29 10:13:25 -0700

    add validation on form

--- src/app/entries/entry/entry-comment-form.component.ts
+++ src/app/entries/entry/entry-comment-form.component.ts
@@ -20,6 +20,7 @@ export class EntryCommentFormComponent {
     }
 
     onSubmit(commentForm: NgForm){
+        if (this.commentForm.invalid) return;
         let comment = { name: this.name, comment: this.comment };
         this.entryService.addComment(this.entryId, comment)
             .then(() => {
